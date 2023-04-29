f64a1beb79d52712e797e381b9c32b6853638f44
fizo-neechan <Faizanknight5@gmail.com>
2023-04-29 21:56:46 +0500

    base

---
+++ Computer.h
@@ -0,0 +1,26 @@
+#include "headers.h"
+
+class Computer {
+private:
+    
+    Register AC(12);
+    Register IR(12);
+    Register PC(12);
+    Register AR(12);
+    Register IN(12);
+    Register OUT(12);
+    
+
+
+public:
+    Computer(/* args */);
+    ~Computer();
+};
+
+Computer::Computer(/* args */)
+{
+}
+
+Computer::~Computer()
+{
+}
